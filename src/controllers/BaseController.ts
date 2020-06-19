333d280fdeb37839b7dd9c449288cb20907559b1
Martin Albert <martin.albert.187@gmail.com>
2020-06-19 16:46:19 +0200

    added new Errors to BaseController

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -37,6 +37,18 @@ export abstract class BaseController {
         return next(new errors.BadRequestError(message ? message : 'Bad Request Error'))
     }
 
+    public invalidContent(next: Next, message?: string) {
+        return next(
+            new errors.InvalidContentError(message ? message : 'Invalid content Error')
+        )
+    }
+
+    public requestExpired(next: Next, message?: string) {
+        return next(
+            new errors.RequestExpiredError(message ? message : 'Request expired error')
+        )
+    }
+
     public unauthorized(next: Next, message?: string) {
         return next(new errors.UnauthorizedError(message ? message : 'Unauthorized'))
     }
@@ -55,6 +67,10 @@ export abstract class BaseController {
         return next(new errors.NotFoundError(message ? message : 'Not found'))
     }
 
+    public resourceNotFound(next: Next, message?: string) {
+        return next(new errors.NotFoundError(message ? message : 'Resource not found'))
+    }
+
     public conflict(next: Next, message?: string) {
         return next(new errors.ConflictError(message ? message : 'Conflict'))
     }
@@ -62,12 +78,6 @@ export abstract class BaseController {
     public tooMany(next: Next, message?: string) {
         return next(
             new errors.TooManyRequestsError(message ? message : 'Too many requests')
-        )
-    }
-
-    public requestExpired(next: Next, message?: string) {
-        return next(
-            new errors.RequestExpiredError(message ? message : 'Request expired error')
         )
     }
 
