c48772b66688f33250b7589fd7a93c43be12f543
Martin Albert <martin.albert.187@gmail.com>
2020-06-19 16:45:09 +0200

    added new enviroment variables for mysql

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 export default {
     ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 3000,
     URL: process.env.BASE_URL || 'http://localhost:3000',
-    MYSQL_URI: process.env.MYSQL_URI || 'something',
-    CURRENT_DATABASE: 'mysql',
+    PORT: process.env.PORT || 3000,
+    CURRENT_DATABASE: process.env.CURRENT_DATABASE || 'mysql',
+    MYSQL_URL: process.env.MYSQL_URI || 'localhost',
+    MYSQL_PORT: Number(process.env.MYSQL_PORT) || 8889,
+    MYSQL_DB: process.env.MYSQL_DB || 'database',
+    MYSQL_USER: process.env.MYSQL_USER || 'root',
+    MYSQL_PW: process.env.MYSQL_PW || 'root',
 }
