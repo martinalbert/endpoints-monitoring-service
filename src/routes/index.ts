9b3620ee56d2ddb60c2eac7293f7d704f9c71c09
Martin Albert <martin.albert.187@gmail.com>
2020-06-19 16:51:33 +0200

    added User routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,23 +6,26 @@ import {
     updateMonitoredEndpoint,
     deleteMonitoredEndpoint,
 } from '../useCases/MonitoredEndpointUseCases'
+import { registerUser, loginUser } from '../useCases/UserUseCases'
 
 export default (server: Server) => {
-    console.log('setting up routes')
-
     server.get('/endpoints', async (req, res, next) =>
         getAllMonitoredEndpoints.exec(req, res, next)
     )
-    server.get('/endpoints/:ID', async (req, res, next) =>
+    server.get('/endpoints/:id', async (req, res, next) =>
         getMonitoredEndpointByID.exec(req, res, next)
     )
     server.post('/endpoints', async (req, res, next) =>
         createMonitoredEndpoint.exec(req, res, next)
     )
-    server.patch('/endpoints/:ID', async (req, res, next) =>
+    server.put('/endpoints/:id', async (req, res, next) =>
         updateMonitoredEndpoint.exec(req, res, next)
     )
-    server.del('/endpoints/:ID', async (req, res, next) =>
+    server.del('/endpoints/:id', async (req, res, next) =>
         deleteMonitoredEndpoint.exec(req, res, next)
     )
+    server.post('/user/register', async (req, res, next) =>
+        registerUser.exec(req, res, next)
+    )
+    server.post('/user/login', async (req, res, next) => loginUser.exec(req, res, next))
 }
