8418aae3716d8d1b30732e50ef41c6c8b18a6af1
Martin Albert <martin.albert.187@gmail.com>
2020-06-19 16:52:07 +0200

    create user useCase

---
+++ src/useCases/UserUseCases.ts
@@ -0,0 +1,10 @@
+import {
+    UserRegisterController,
+    UserLoginController,
+} from '../controllers/UserController'
+import { UserRepo } from '../daos'
+
+const userRepo = new UserRepo()
+
+export const registerUser = new UserRegisterController(userRepo)
+export const loginUser = new UserLoginController(userRepo)
