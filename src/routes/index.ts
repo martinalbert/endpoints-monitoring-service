adbe975ea961ed6cfd7048c94c7808c82d0e205c
Martin Albert <martin.albert.187@gmail.com>
2020-06-21 16:32:41 +0200

    added other routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
 import { Server } from 'restify'
+import { registerUser, loginUser } from '../useCases/UserUseCases'
+import checkAuth from '../utils/checkAuth'
 import {
     getMonitoredEndpointByID,
     getAllMonitoredEndpoints,
@@ -6,26 +8,43 @@ import {
     updateMonitoredEndpoint,
     deleteMonitoredEndpoint,
 } from '../useCases/MonitoredEndpointUseCases'
-import { registerUser, loginUser } from '../useCases/UserUseCases'
+import {
+    getMonitoringResultByID,
+    getAllMonitoringResults,
+    createMonitoringResult,
+    deleteMonitoringResult,
+} from '../useCases/MonitoringResultUseCases'
 
 export default (server: Server) => {
-    server.get('/endpoints', async (req, res, next) =>
+    server.post('/user/login', async (req, res, next) => loginUser.exec(req, res, next))
+    server.post('/user/register', async (req, res, next) => registerUser.exec(req, res, next))
+
+    server.get('/endpoints', checkAuth, async (req, res, next) =>
         getAllMonitoredEndpoints.exec(req, res, next)
     )
-    server.get('/endpoints/:id', async (req, res, next) =>
+    server.get('/endpoints/:id', checkAuth, async (req, res, next) =>
         getMonitoredEndpointByID.exec(req, res, next)
     )
-    server.post('/endpoints', async (req, res, next) =>
+    server.post('/endpoints', checkAuth, async (req, res, next) =>
         createMonitoredEndpoint.exec(req, res, next)
     )
-    server.put('/endpoints/:id', async (req, res, next) =>
+    server.put('/endpoints/:id', checkAuth, async (req, res, next) =>
         updateMonitoredEndpoint.exec(req, res, next)
     )
-    server.del('/endpoints/:id', async (req, res, next) =>
+    server.del('/endpoints/:id', checkAuth, async (req, res, next) =>
         deleteMonitoredEndpoint.exec(req, res, next)
     )
-    server.post('/user/register', async (req, res, next) =>
-        registerUser.exec(req, res, next)
+
+    server.get('/endpoints/:eID/results', checkAuth, async (req, res, next) =>
+        getAllMonitoringResults.exec(req, res, next)
     )
-    server.post('/user/login', async (req, res, next) => loginUser.exec(req, res, next))
+    server.get('/endpoints/:eID/results/:id', checkAuth, async (req, res, next) =>
+        getMonitoringResultByID.exec(req, res, next)
+    )
+    server.post('/endpoints/:eID/results', checkAuth, async (req, res, next) =>
+        createMonitoringResult.exec(req, res, next)
+    )
+    server.del('/endpoints/:eID/results/:id', checkAuth, async (req, res, next) =>
+        deleteMonitoringResult.exec(req, res, next)
+    )
 }
