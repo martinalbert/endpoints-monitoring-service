859c3ee9ab798edd9e4e4c0ad362913e8025571f
Martin Albert <martin.albert.187@gmail.com>
2020-06-21 16:28:31 +0200

    modified interfaces of repos

--- src/daos/repos/IUserRepo.ts
+++ src/daos/repos/IUserRepo.ts
@@ -1,11 +1,11 @@
 import User from '../../entities/User'
 
 export default abstract class IUserRepo {
-    abstract async getByID(id: number): Promise<User>
-    abstract async getCurrent(accessToken: string): Promise<User>
-    abstract async getAll(): Promise<User[]>
     abstract async register(user: User): Promise<User>
-    abstract async login(user: User): Promise<User>
-    abstract async update(id: Number, user: User): Promise<boolean>
-    abstract async delete(id: Number): Promise<boolean>
+    abstract async login(email: string, pw: string): Promise<object>
+    abstract async getByID(id: number): Promise<User>
+    abstract async getCurrent(id: number, email: string): Promise<User>
+    abstract async getAll(): Promise<User[]>
+    // abstract async update(id: Number, user: User): Promise<boolean>
+    // abstract async delete(id: Number): Promise<boolean>
 }
