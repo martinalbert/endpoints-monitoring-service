174a785e280ea36f1771c844b255156b04e18d71
Martin Albert <martin.albert.187@gmail.com>
2020-06-22 15:36:20 +0200

    moved root files to seperate directory as rest api server

--- src/daos/repos/IMonitoredEndpointRepo.ts
+++
@@ -1,9 +0,0 @@
-import MonitoredEndpoint from '../../entities/MonitoredEndpoint'
-
-export default abstract class IMonitoredEndpointRepo {
-    abstract async getByID(id: number, uID: number): Promise<MonitoredEndpoint>
-    abstract async getAll(uID: number): Promise<MonitoredEndpoint[]>
-    abstract async create(monitoredEndpoint: MonitoredEndpoint): Promise<MonitoredEndpoint>
-    abstract async update(id: number, monitoredEndpoint: MonitoredEndpoint): Promise<boolean>
-    abstract async delete(id: number, uID: number): Promise<boolean>
-}
--- src/useCases/UserUseCases.ts
+++
@@ -1,7 +0,0 @@
-import { UserRegisterController, UserLoginController } from '../controllers/UserController'
-import { UserRepo } from '../daos'
-
-const userRepo = new UserRepo()
-
-export const registerUser = new UserRegisterController(userRepo)
-export const loginUser = new UserLoginController(userRepo)
