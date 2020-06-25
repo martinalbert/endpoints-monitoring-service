d129a8e41f0787a86f15405a10fc46f77e390337
Martin Albert <martin.albert.187@gmail.com>
2020-06-25 19:43:43 +0200

    added new Controller to useCase

--- server/src/useCases/UserUseCases.ts
+++ server/src/useCases/UserUseCases.ts
@@ -1,7 +1,12 @@
-import { UserRegisterController, UserLoginController } from '../controllers/UserController'
-import { UserRepo } from '../daos'
+import {
+    UserRegisterController,
+    UserLoginController,
+    GetAllUsersController,
+} from '../controllers/UserController'
+import { UserRepo } from '../db'
 
 const userRepo = new UserRepo()
 
 export const registerUser = new UserRegisterController(userRepo)
 export const loginUser = new UserLoginController(userRepo)
+export const getAllUsersController = new GetAllUsersController(userRepo)
