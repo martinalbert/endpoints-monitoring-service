cefece33e5e9255bb143fb754ce91cf67c76a009
Martin Albert <martin.albert.187@gmail.com>
2020-06-29 03:02:14 +0200

    modified semantics of route handler

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,54 +1,86 @@
 import { Server } from 'restify'
-import { registerUser, loginUser, getAllUsersController } from '../useCases/UserUseCases'
+import { register, login, getAllUsers } from '../useCases/UserUseCases'
 import checkAuth from '../utils/checkAuth'
 import {
-    getMonitoredEndpointByID,
-    getAllMonitoredEndpoints,
-    createMonitoredEndpoint,
-    updateMonitoredEndpoint,
-    deleteMonitoredEndpoint,
+    getEndpointByID,
+    getAllEndpoints,
+    createEndpoint,
+    updateEndpoint,
+    deleteEndpoint,
 } from '../useCases/MonitoredEndpointUseCases'
 import {
-    getMonitoringResultByID,
-    getAllMonitoringResults,
-    createMonitoringResult,
-    deleteMonitoringResult,
+    getResultByID,
+    getAllResults,
+    createResult,
+    deleteResult,
 } from '../useCases/MonitoringResultUseCases'
 
+/**
+ * Routes Handler\
+ * Function that handles all API Endpoints for specified HTTP Server.
+ *
+ * @function routes
+ * @param {Server} server - Restify HTTP Server
+ */
 export default (server: Server) => {
-    server.get('/users', checkAuth, async (req, res, next) =>
-        getAllUsersController.exec(req, res, next)
+    // Users API Endpoints
+    server.get(
+        '/users',
+        checkAuth,
+        async (req, res, next) => await getAllUsers.exec(req, res, next)
+    )
+    server.post(
+        '/user/register',
+        async (req, res, next) => await register.exec(req, res, next)
+    )
+    server.post('/user/login', async (req, res, next) => await login.exec(req, res, next))
+
+    // Endpoints API Endpoints
+    server.get(
+        '/endpoints',
+        checkAuth,
+        async (req, res, next) => await getAllEndpoints.exec(req, res, next)
+    )
+    server.get(
+        '/endpoints/:id',
+        checkAuth,
+        async (req, res, next) => await getEndpointByID.exec(req, res, next)
+    )
+    server.post(
+        '/endpoints',
+        checkAuth,
+        async (req, res, next) => await createEndpoint.exec(req, res, next)
+    )
+    server.put(
+        '/endpoints/:id',
+        checkAuth,
+        async (req, res, next) => await updateEndpoint.exec(req, res, next)
+    )
+    server.del(
+        '/endpoints/:id',
+        checkAuth,
+        async (req, res, next) => await deleteEndpoint.exec(req, res, next)
     )
 
-    server.post('/user/login', async (req, res, next) => loginUser.exec(req, res, next))
-    server.post('/user/register', async (req, res, next) => registerUser.exec(req, res, next))
-
-    server.get('/endpoints', checkAuth, async (req, res, next) =>
-        getAllMonitoredEndpoints.exec(req, res, next)
+    // Results API Endpoints
+    server.get(
+        '/endpoints/:eID/results',
+        checkAuth,
+        async (req, res, next) => await getAllResults.exec(req, res, next)
     )
-    server.get('/endpoints/:id', checkAuth, async (req, res, next) =>
-        getMonitoredEndpointByID.exec(req, res, next)
+    server.get(
+        '/endpoints/:eID/results/:id',
+        checkAuth,
+        async (req, res, next) => await getResultByID.exec(req, res, next)
     )
-    server.post('/endpoints', checkAuth, async (req, res, next) =>
-        createMonitoredEndpoint.exec(req, res, next)
+    server.post(
+        '/endpoints/:eID/results',
+        checkAuth,
+        async (req, res, next) => await createResult.exec(req, res, next)
     )
-    server.put('/endpoints/:id', checkAuth, async (req, res, next) =>
-        updateMonitoredEndpoint.exec(req, res, next)
-    )
-    server.del('/endpoints/:id', checkAuth, async (req, res, next) =>
-        deleteMonitoredEndpoint.exec(req, res, next)
-    )
-
-    server.get('/endpoints/:eID/results', checkAuth, async (req, res, next) =>
-        getAllMonitoringResults.exec(req, res, next)
-    )
-    server.get('/endpoints/:eID/results/:id', checkAuth, async (req, res, next) =>
-        getMonitoringResultByID.exec(req, res, next)
-    )
-    server.post('/endpoints/:eID/results', checkAuth, async (req, res, next) =>
-        createMonitoringResult.exec(req, res, next)
-    )
-    server.del('/endpoints/:eID/results/:id', checkAuth, async (req, res, next) =>
-        deleteMonitoringResult.exec(req, res, next)
+    server.del(
+        '/endpoints/:eID/results/:id',
+        checkAuth,
+        async (req, res, next) => await deleteResult.exec(req, res, next)
     )
 }
