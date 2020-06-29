5003ec24f574a8353541aae6bd2082ab3bc1be31
Martin Albert <martin.albert.187@gmail.com>
2020-06-29 02:53:29 +0200

    added jwtObject interface to local @types

--- server/src/@types/index.d.ts
+++ server/src/@types/index.d.ts
@@ -129,3 +129,32 @@ interface IMonitoredEndpoint {
      */
     toObject(): Object
 }
+
+/**
+ * Interface\
+ * Interface for Object that is decoded by valid json web token
+ *
+ * @interface
+ * @param  {number} id - id of encoded User
+ * @param  {string} email - email of encoded User
+ * @param  {number} iat - issued at - time at which the JWT was created
+ * @param  {number} exp - expiration time - time on which JWT won't be accepted
+ */
+interface jwtObject {
+    /**
+     * id of encoded User
+     */
+    id: number
+    /**
+     * email of encoded User
+     */
+    email: string
+    /**
+     * issued at - time at which the JWT was created
+     */
+    iat: number
+    /**
+     * expiration time - time on which JWT won't be accepted
+     */
+    exp: number
+}
