c3d3781a0d43b6ececc1837f49fadbb95fcce2c6
Martin Albert <martin.albert.187@gmail.com>
2020-06-29 02:58:32 +0200

    commented abstractions of repos

--- server/src/db/repos/IMonitoredEndpointRepo.ts
+++ server/src/db/repos/IMonitoredEndpointRepo.ts
@@ -1,9 +1,53 @@
 import MonitoredEndpoint from '../../entities/MonitoredEndpoint'
 
+/**
+ * Abstract Class\
+ * Contains abstractions of all functions that communicate with database
+ *
+ * @abstract @class IMonitoredEndpointRepo
+ * @function getByID - Function that finds one endpoint represented by its its ID
+ * @function getAll - Function that finds all endpoints corresponding to its owner
+ * @function create - Function that creates new endpoint corresponding to endpoint passed in
+ * @function update - Function that updates old endpoint corresponding to endpoint passed in
+ * @function delete - Function that deletes endpoint represented by its ID
+ */
 export default abstract class IMonitoredEndpointRepo {
+    /**
+     * Function that finds one endpoint represented by its its ID
+     * @abstract @async @function getByID
+     * @param {number} id - ID of Endpoint
+     * @param {number} uID - ID of User
+     * @returns {Promise<MonitoredEndpoint>} found Monitored Endpoint
+     */
     abstract async getByID(id: number, uID: number): Promise<MonitoredEndpoint>
+    /**
+     * Function that finds all endpoints corresponding to its owner
+     * @abstract @async @function getAll
+     * @param {number} uID - ID of User
+     * @returns {Promise<MonitoredEndpoint[]>} found Monitored Endpoints
+     */
     abstract async getAll(uID: number): Promise<MonitoredEndpoint[]>
+    /**
+     * Function that creates new endpoint corresponding to endpoint passed in
+     * @abstract @async @function create
+     * @param {MonitoredEndpoint} monitoredEndpoint - endpoint that is going to be created
+     * @returns {Promise<MonitoredEndpoint>} created Monitored Endpoint
+     */
     abstract async create(monitoredEndpoint: MonitoredEndpoint): Promise<MonitoredEndpoint>
+    /**
+     * Function that updates old endpoint corresponding to endpoint passed in
+     * @abstract @async @function update
+     * @param {number} id - ID of Endpoint
+     * @param {MonitoredEndpoint} monitoredEndpoint - endpoint that is going to be updated
+     * @returns {Promise<boolean>} value that indicates whether endpoint was updated or not
+     */
     abstract async update(id: number, monitoredEndpoint: MonitoredEndpoint): Promise<boolean>
+    /**
+     * Function that deletes endpoint represented by its ID
+     * @abstract @async @function delete
+     * @param {number} id - ID of Endpoint
+     * @param {number} uID - ID of User
+     * @returns {Promise<boolean>} value that indicates whether endpoint was deleted or not
+     */
     abstract async delete(id: number, uID: number): Promise<boolean>
 }
--- server/src/db/repos/IMonitoringResultRepo.ts
+++ server/src/db/repos/IMonitoringResultRepo.ts
@@ -1,10 +1,53 @@
 import MonitoringResult from '../../entities/MonitoringResult'
 import MonitoredEndpoint from '../../entities/MonitoredEndpoint'
 
+/**
+ * Abstract Class\
+ * Contains abstractions of all functions that communicate with database
+ *
+ * @abstract @class IMonitoringResultRepo
+ * @function getByID - Function that finds one result represented by its ID
+ * @function getLast10 - Function that finds last 10 results corresponding to endpoint that is being monitored
+ * @function getAll - Function that finds all results corresponding to endpoint that is being monitored
+ * @function create - Function that creates new result corresponding to result passed in
+ * @function delete - Function that deletes result represented by its its ID
+ */
 export default abstract class IMonitoringResultRepo {
+    /**
+     * Function that finds one result represented by its ID
+     * @abstract @async @function getByID
+     * @param {number} id - ID of Result
+     * @param {MonitoredEndpoint} endpoint - endpoint that is being monitored
+     * @returns {Promise<MonitoringResult>} found Monitoring Result
+     */
     abstract async getByID(id: Number, endpoint: MonitoredEndpoint): Promise<MonitoringResult>
+    /**
+     * Function that finds last 10 results corresponding to endpoint that is being monitored
+     * @abstract @async @function getLast10
+     * @param {MonitoredEndpoint} endpoint - endpoint that is being monitored
+     * @returns {Promise<MonitoringResult[]>} found Monitoring Results
+     */
     abstract async getLast10(endpoint: MonitoredEndpoint): Promise<MonitoringResult[]>
+    /**
+     * Function that finds all results corresponding to endpoint that is being monitored
+     * @abstract @async @function getAll
+     * @param {MonitoredEndpoint} endpoint - endpoint that is being monitored
+     * @returns {Promise<MonitoringResult[]>} found Monitoring Results
+     */
     abstract async getAll(endpoint: MonitoredEndpoint): Promise<MonitoringResult[]>
+    /**
+     * Function that creates new result corresponding to result passed in
+     * @abstract @async @function create
+     * @param {MonitoredEndpoint} endpoint - endpoint that is being monitored
+     * @returns {Promise<MonitoringResult>} created Monitoring Result
+     */
     abstract async create(monitoringResult: MonitoringResult): Promise<MonitoringResult>
+    /**
+     * Function that deletes result represented by its its ID
+     * @abstract @async @function delete
+     * @param {number} id - ID of Result
+     * @param {MonitoredEndpoint} endpoint - endpoint that is being monitored
+     * @returns {Promise<boolean>} value that indicates whether result was deleted or not
+     */
     abstract async delete(id: Number, endpoint: MonitoredEndpoint): Promise<boolean>
 }
