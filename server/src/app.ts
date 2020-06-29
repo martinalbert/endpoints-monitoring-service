5fb0eb3c673cfa5beb4df2c4d64ed3bf64c760e1
Martin Albert <martin.albert.187@gmail.com>
2020-06-29 02:54:51 +0200

    commented app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,13 +2,17 @@ import restify from 'restify'
 import morgan from 'morgan'
 import routes from './routes'
 
+/**
+ * Restify Server\
+ * Listens on Port 3000
+ */
 const server = restify.createServer()
 
 // Middlewares & plugins
 server.use(restify.plugins.bodyParser())
 server.use(morgan('dev'))
 
-// Routes Handlers
+// Routes Handler
 routes(server)
 
 export default server
