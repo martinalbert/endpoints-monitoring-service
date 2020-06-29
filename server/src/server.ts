70246933df84d2380f37b38b617d0cce05a683bc
Martin Albert <martin.albert.187@gmail.com>
2020-06-29 02:53:07 +0200

    added seed function right after connection function

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,13 @@
 import config from './config'
 import app from './app'
 import { connect } from './db'
+import seed from './db/repos/mysql/seed'
+import clearDB from './db/repos/mysql/clearDB'
 
+/**
+ * Restify Server\
+ * Listens on Port 3000
+ */
 const server = app
 
 server.listen(config.PORT, () => {
@@ -9,6 +15,13 @@ server.listen(config.PORT, () => {
 
     // connect to DB (mySQL)
     connect()
+
+    // seed sample data
+    seed()
+        .then(() => console.log('data have been seeded'))
+        .catch(err => {
+            throw Error(err)
+        })
 })
 
 server.on('error', (err: Error) => {
