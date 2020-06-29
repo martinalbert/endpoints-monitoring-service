b933c57cd64f7d9a214c8f450231debee3afcd7d
Martin Albert <martin.albert.187@gmail.com>
2020-06-29 02:57:55 +0200

    commented exports

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,8 +1,63 @@
 import loader from './loader'
 
+/**
+ * Repository Class\
+ * Entity: User\
+ * Class that handles communication with database
+ *
+ * @class UserRepo
+ * @extends IUserRepo
+ * @function register - Function that creates(register) new User
+ * @function login - Function that logs in user which is authenticated
+ * @function getCurrent - Function that finds user that is currently loged in
+ * @function getAll - Function that finds all users
+ */
 export const UserRepo = loader.loadRepo('user')
+
+/**
+ * Repository Class\
+ * Entity: Monitored Endpoint\
+ * Class that handles communication with database
+ *
+ * @class MonitoredEndpointRepo
+ * @extends IMonitoredEndpointRepo
+ * @function getByID - Function that finds one endpoint represented by its its ID
+ * @function getAll - Function that finds all endpoints corresponding to its owner
+ * @function create - Function that creates new endpoint corresponding to endpoint passed in
+ * @function update - Function that updates old endpoint corresponding to endpoint passed in
+ * @function delete - Function that deletes endpoint represented by its ID
+ */
 export const MonitoredEndpointRepo = loader.loadRepo('monitoredEndpoint')
+
+/**
+ * Repository Class\
+ * Entity: Monitoring Result\
+ * Class that handles communication with database
+ *
+ * @class MonitoringResultRepo
+ * @extends IMonitoringResultRepo
+ * @function getByID - Function that finds one result represented by its ID
+ * @function getLast10 - Function that finds last 10 results corresponding to endpoint that is being monitored
+ * @function getAll - Function that finds all results corresponding to endpoint that is being monitored
+ * @function create - Function that creates new result corresponding to result passed in
+ * @function delete - Function that deletes result represented by its its ID
+ */
 export const MonitoringResultRepo = loader.loadRepo('monitoringResult')
 
+/**
+ * Instance of Sequelize\
+ * Represent connection to database.
+ *
+ * @instance of Sequelize
+ */
 export const { mysql } = loader.loadClient()
+/**
+ * Function that authenticate connection to database and synchronize tables.
+ * @function connect
+ */
 export const { connect } = loader.loadClient()
+/**
+ * Function that drops all tables from database
+ * @function dropAllTables
+ */
+export const { dropAllTables } = loader.loadClient()
--- server/src/db/loader.ts
+++ server/src/db/loader.ts
@@ -2,14 +2,20 @@ import config from '../config'
 
 /**
  * Load an implementation of a specified dao(repo).
+ * @function loadRepo
  * @param {string} daoName - String of specified Data Access Object
- * @returns {module} Built in require function that returns some module - in this case implementatioon of DAO module
+ * @returns {module} implementatioon of DAO module
  */
 const loadRepo = (daoName: string) => {
     const currentDatabase = config.CURRENT_DATABASE
     return require(`./repos/${currentDatabase}/${daoName}_${currentDatabase}_repo`).default
 }
 
+/**
+ * Load an implementation of a specified database client.
+ * @function loadClient
+ * @returns {module} implementatioon of database client
+ */
 const loadClient = () => {
     const currentDatabase = config.CURRENT_DATABASE
     return require(`./repos/${currentDatabase}/${currentDatabase}Client`)
