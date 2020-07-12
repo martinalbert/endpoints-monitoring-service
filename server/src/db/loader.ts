de6801020c1c79dd6dee227cff5ae59013fab8af
Martin Albert <martin.albert.187@gmail.com>
2020-07-12 19:34:43 +0200

    removed unnecessary code

--- server/src/db/loader.ts
+++ server/src/db/loader.ts
@@ -11,17 +11,6 @@ const loadRepo = (daoName: string) => {
     return require(`./repos/${currentDatabase}/${daoName}_${currentDatabase}_repo`).default
 }
 
-/**
- * Load an implementation of a specified database client.
- * @function loadClient
- * @returns {module} implementatioon of database client
- */
-const loadClient = () => {
-    const currentDatabase = config.CURRENT_DATABASE
-    return require(`./repos/${currentDatabase}/${currentDatabase}Client`)
-}
-
 export default {
     loadRepo,
-    loadClient,
 }
