7a7f11b2589aa1d5cca3f2ca925b5fbb41aa3b99
Martin Albert <martin.albert.187@gmail.com>
2020-07-12 20:04:43 +0200

    fixed queries and semantics

--- server/src/db/repos/mysql/actions/index.ts
+++ server/src/db/repos/mysql/actions/index.ts
@@ -2,14 +2,14 @@ import { QueryTypes } from 'sequelize'
 import sequelize from '../../../sequelize'
 import { MonitoredEndpointRepo, UserRepo } from '../../../../db'
 
-const userRepo = new UserRepo()
-const endpointRepo = new MonitoredEndpointRepo()
-
 /**
  * Function that finds all users and deletes all theirs endpoints and results.
  * @async @function clearDB
  */
 export const clearDB = async () => {
+    const userRepo = new UserRepo()
+    const endpointRepo = new MonitoredEndpointRepo()
+
     const users = await userRepo.getAll()
     for (const user of users) {
         const endpoints = await endpointRepo.getAll(user.id)
@@ -29,22 +29,31 @@ export const dropAllTables = () => {
 
 /**
  * Helper Function\
- * Function that gets the last ID of users and monitoredEndpoints.
+ * Function that gets the last ID of users, monitoredEndpoints and monitoringResults.
  *
  * @async @function getLastID
  * @return {Object} object with user last ID and endpoint last ID
  */
 export const getLastID = async () => {
     const endpointCount = await sequelize.query(
-        'SELECT setval(\'monitoredEndpoints_id_seq\', (SELECT MAX(id) FROM "monitoredEndpoints"));',
-        { type: QueryTypes.SELECT }
+        'SELECT MAX(id) AS count FROM monitoredEndpoints;',
+        {
+            type: QueryTypes.SELECT,
+        }
     )
-    const userCount = await sequelize.query(
-        'SELECT setval(\'users_id_seq\', (SELECT MAX(id) FROM "users"));',
-        { type: QueryTypes.SELECT }
+    const resultCount = await sequelize.query(
+        'SELECT MAX(id) AS count FROM monitoringResults;',
+        {
+            type: QueryTypes.SELECT,
+        }
     )
+    const userCount = await sequelize.query('SELECT MAX(id) AS count FROM users;', {
+        type: QueryTypes.SELECT,
+    })
+
     return {
-        endpointCount: Number(endpointCount[0].setval),
-        userCount: Number(userCount[0].setval),
+        userCount: userCount[0].count,
+        endpointCount: endpointCount[0].count,
+        resultCount: resultCount[0].count,
     }
 }
